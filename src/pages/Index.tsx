577720649a05e1724a5962f04e5032be221cf29d
audreybousquet-a11y <audreybousquet@abcarre.fr>
2026-04-07 16:58:01 +0200

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,23 +1,21 @@
-import Navbar from "@/components/Navbar";
-import HeroSection from "@/components/HeroSection";
-import FeaturesSection from "@/components/FeaturesSection";
-import HowItWorksSection from "@/components/HowItWorksSection";
-import PricingSection from "@/components/PricingSection";
-import ContactSection from "@/components/ContactSection";
-import Footer from "@/components/Footer";
+  import Navbar from "@/components/Navbar";
+  import HeroSection from "@/components/HeroSection";
+  import FeaturesSection from "@/components/FeaturesSection";
+  import HowItWorksSection from "@/components/HowItWorksSection";
+  import ContactSection from "@/components/ContactSection";
+  import Footer from "@/components/Footer";
 
-const Index = () => (
-  <>
-    <Navbar />
-    <main>
-      <HeroSection />
-      <FeaturesSection />
-      <HowItWorksSection />
-      <PricingSection />
-      <ContactSection />
-    </main>
-    <Footer />
-  </>
-);
+  const Index = () => (
+    <>
+      <Navbar />
+      <main>
+        <HeroSection />
+        <FeaturesSection />
+        <HowItWorksSection />
+        <ContactSection />
+      </main>
+      <Footer />
+    </>
+  );
 
-export default Index;
+  export default Index;
