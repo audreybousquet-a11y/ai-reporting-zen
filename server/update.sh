1426307e8baa5dfe826d3393d7c9139a9c916f3c
audreybousquet-a11y <audreybousquet@abcarre.fr>
2026-04-07 22:27:22 +0200

    Update script with logo

--- server/update.sh
+++ server/update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 BASE=https://raw.githubusercontent.com/audreybousquet-a11y/ai-reporting-zen/main/server/aria
 wget --no-cache -q $BASE/aria.py -O /home/ubuntu/aria/app/aria.py
+wget --no-cache -q $BASE/logo-aria.png -O /home/ubuntu/aria/app/logo-aria.png
 sudo systemctl restart aria_streamlit
 echo "OK"
