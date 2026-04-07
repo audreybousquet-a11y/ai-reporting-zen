e51475547fdfad2b493e8ed7f9ef66464956ba79
audreybousquet-a11y <audreybousquet@abcarre.fr>
2026-04-07 23:12:06 +0200

    Update src/pages/Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,21 +1,23 @@
-  import Navbar from "@/components/Navbar";
-  import HeroSection from "@/components/HeroSection";
-  import FeaturesSection from "@/components/FeaturesSection";
-  import HowItWorksSection from "@/components/HowItWorksSection";
-  import ContactSection from "@/components/ContactSection";
-  import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import FeaturesSection from "@/components/FeaturesSection";
+import HowItWorksSection from "@/components/HowItWorksSection";
+import TrustSection from "@/components/TrustSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
 
-  const Index = () => (
-    <>
-      <Navbar />
-      <main>
-        <HeroSection />
-        <FeaturesSection />
-        <HowItWorksSection />
-        <ContactSection />
-      </main>
-      <Footer />
-    </>
-  );
+const Index = () => (
+  <>
+    <Navbar />
+    <main>
+      <HeroSection />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <TrustSection />
+      <ContactSection />
+    </main>
+    <Footer />
+  </>
+);
 
-  export default Index;
+export default Index;
