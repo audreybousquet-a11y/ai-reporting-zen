4ed04ebebaf55dd402cd15d58416890f632bb060
audreybousquet-a11y <audreybousquet@abcarre.fr>
2026-04-07 22:25:01 +0200

    Update script

--- server/update.sh
+++ server/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-wget -q https://raw.githubusercontent.com/audreybousquet-a11y/ai-reporting-zen/main/server/aria/aria.py -O /home/ubuntu/aria/app/aria.py
+BASE=https://raw.githubusercontent.com/audreybousquet-a11y/ai-reporting-zen/main/server/aria
+wget --no-cache -q $BASE/aria.py -O /home/ubuntu/aria/app/aria.py
 sudo systemctl restart aria_streamlit
 echo "OK"
